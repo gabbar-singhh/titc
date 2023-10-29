b6a4e369a05d297e007e386ae68304795860b159
gabbar <himanshu01.dev@gmail.com>
2023-10-29 23:23:24 +0530

    react-image-gallery pkg removed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }) {
 
         {/* GOOGLE FONTS */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&family=Roboto:wght@700&display=swap"
           rel="stylesheet"
