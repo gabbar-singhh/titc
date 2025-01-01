c5edef4612fcf75cfee47d6a31e1e75d9ff36a97
gabbar-singhh <himanshup1308@gmail.com>
2025-01-01 16:24:28 +0530

    :home stats section -done

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
         <CompareSection />
         <ServicesSection />
         <Timeline />
-        {/* <StatsSection /> */}
+        <StatsSection />
         {/* <ContactUsSection /> */}
         {/* <FooterSection /> */}
       </section>
