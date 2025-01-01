052dd7eb30291c241ffe35dff13f75ae48f8b94a
gabbar-singhh <himanshup1308@gmail.com>
2025-01-01 18:11:58 +0530

    service page :done

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
         <Timeline />
         <StatsSection />
         <ContactUsSection />
-        {/* <FooterSection /> */}
+        <FooterSection />
       </section>
     </>
   );
