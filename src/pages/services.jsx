4a89e43825a7b6f167685ba579d0b8f667635253
gabbar-singhh <himanshup1308@gmail.com>
2025-01-06 04:38:22 +0530

    :service page respo -done

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import styles from "@/styles/Services.module.css";
 import ContactUsSection from "@/Components/ContactUsSection/ContactUsSection";
