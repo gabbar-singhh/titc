0045be8eae42246aef96dc329a3f08153c6a27fb
gabbar-singhh <himanshup1308@gmail.com>
2025-01-06 10:29:46 +0530

    :service section respo -done

--- src/components/ServicesSection/ServicesSection.jsx
+++ src/components/ServicesSection/ServicesSection.jsx
@@ -7,10 +7,18 @@ const ServicesSection = () => {
   return (
     <div className={styles.main}>
       <div class={styles.custom_shape_divider_top_1735330510}>
-    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class={styles.shape_fill}></path>
-    </svg>
-</div>
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+            class={styles.shape_fill}
+          ></path>
+        </svg>
+      </div>
 
       <section className={styles.container}>
         <h2>Our Expert Services</h2>
@@ -79,10 +87,18 @@ const ServicesSection = () => {
       </section>
 
       <div class={styles.custom_shape_divider_bottom_1735330233}>
-    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class={styles.shape_fill}></path>
-    </svg>
-</div>
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+            class={styles.shape_fill}
+          ></path>
+        </svg>
+      </div>
     </div>
   );
 };
