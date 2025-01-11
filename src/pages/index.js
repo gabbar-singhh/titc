e746bf5ac4db06c5e261f077cef570e1d125c08e
gabbar-singhh <himanshup1308@gmail.com>
2025-01-11 15:31:44 +0530

    unused font removed

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,13 +19,6 @@ export default function Home() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.svg" />
-
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Sriracha&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <section className={`${styles.main}`}>
         <HeroSection />
