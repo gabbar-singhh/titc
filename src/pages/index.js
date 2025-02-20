564b30a75091e079b0f5e0faf4b270c70cd9c3bd
gabbar-singhh <himanshup1308@gmail.com>
2025-02-20 10:28:43 +0530

    favicon added to safari

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,6 +36,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.svg" />
 
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon.svg"
+        />
+
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
