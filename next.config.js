004dbd409516b49926c2a32543889e9852d38b05
gabbar-singhh <himanshup1308@gmail.com>
2025-07-16 12:04:30 +0530

    aboutus redirect -done

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/about_us',
+        destination: '/about-us',
+        permanent: true,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
